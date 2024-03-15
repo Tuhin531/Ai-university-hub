5f81f36e9ca5a902ec4f0e7bc979ffea82cbaa9c
Tuhin531 <mdtuhinhossain153@gmail.com>
2024-03-15 02:03:31 +0100

    add images and feature

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 const universityDataLoad = () => {
-    const url =`https://openapi.programming-hero.com/api/ai/tools`
-    
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+
     fetch(url)
         .then(res => res.json())
         .then(status => displayMealData(status.data.tools))
-       
+
 }
-const displayMealData = tools =>{
+const displayMealData = tools => {
 
     //  console.log(meals);
 
     // step 1.  container element
-    const universityContainer =document.getElementById('university-container');
-    universityContainer.innerHTML ='';
+    const universityContainer = document.getElementById('university-container');
+    universityContainer.innerHTML = '';
     tools.forEach(tool => {
         console.log(tool);
         // step 2. create child fro each element
@@ -20,11 +20,17 @@ const displayMealData = tools =>{
         universityDiv.classList.add('col');
         universityDiv.innerHTML = `
         <div class="card h-100">
-                    <img src="" class="card-img-top" alt="...">
+                    <img src="${tool.image}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
+                    <h2>Features</h2>
+                    <ol>
+                    <li>
+                  ${tool.features}
+                    </li>     
+                </ol>
+                    
+                        <h5 class="card-title"> ${tool.name}</h5>
+                        <p class="card-text">${tool.description}</p>
                     </div>
                     <button " type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealModal">
                     Details
@@ -33,7 +39,7 @@ const displayMealData = tools =>{
         </div>
         
         `
-         universityContainer.appendChild(universityDiv);
+        universityContainer.appendChild(universityDiv);
 
     })
 }
